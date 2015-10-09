8dbc08a02021d5c1c8b871c4afa7194a8748bd99
Gregory, Don <DGregory@follett.com>
2015-10-09 13:44:14 -0500

    made mysql deployment option

--- helper.sh
+++ helper.sh
@@ -8,11 +8,16 @@ if [ "$1" = 'capture' ]; then
    exec ./dpxdt/tools/fss_diff_url.py --upload_build_id=$upload_build_id --release_server_prefix=$release_server_prefix --release_client_id=$release_client_id --release_client_secret=$release_client_secret --release_cut_url=$release_cut_url --tests_json_path=$tests_json_path --upload_release_name=$upload_release_name
 
 elif [ "$1" = 'start' ]; then
+   make mysql_deploy
+   cd mysql_deploy
+   virtualenv .
+   ./run.sh
+   #exec ./run_combined.sh $@ 
+elif [ "$1" = 'start_sqlite' ]; then
    make sqlite_deploy
    cd sqlite_deploy
    virtualenv .
    ./run.sh
-   #exec ./run_combined.sh $@ 
 else 
    echo Additional options: start, capture
    echo When start is used, you may optionally add these --ignore_auth, --verbose_workers=true, --verbose_queries=true
